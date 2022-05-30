a57f17f25eea1747aae3f268128b7627fcb4a89a
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-31 00:33:40 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace while_foreach // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            // 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //'a' dan 'z' ye kadar bütün harfleri sırayla yazan program
+            char karakter = 'a';
+            while (karakter<='z')
+            {
+                Console.Write(karakter);
+                karakter++;
+            }
+        }
+    }
+}
